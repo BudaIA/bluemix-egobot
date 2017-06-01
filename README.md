d5ced97676f6b538cc8688e22edf4057a286ba47
data-henrik <henrik.loeser@gmail.com>
2017-06-01 14:54:40 +0200

    upload sources

---
+++ README.md
@@ -0,0 +1,24 @@
+# Egobot
+## A slightly mutating ego-centric chatbot
+
+
+
+# Documentation and Resources
+Here are some useful links to documentation and other resources:
+* Watson Conversation service: https://www.ibm.com/watson/developercloud/doc/conversation/index.html
+* API for Watson Conversation service: https://www.ibm.com/watson/developercloud/conversation/api/v1/#introduction
+* Python SDK, Watson Developer Cloud: https://github.com/watson-developer-cloud/python-sdk
+* Conversation API file in Python SDK: https://github.com/watson-developer-cloud/python-sdk/blob/master/watson_developer_cloud/conversation_v1.py
+* The `argparse` module used for this tool: https://docs.python.org/2/library/argparse.html
+
+
+# License
+See [LICENSE](LICENSE) for license information.
+
+The tool is provided on a "as-is" basis and is un-supported. Use with care...
+
+# Contribute / Contact Information
+If you have found errors or some instructions are not working anymore, then please open an GitHub issue or, better, create a pull request with your desired changes.
+
+You can find more tutorials and sample code at:
+https://ibm-bluemix.github.io/
---
+++ egobot.py
@@ -0,0 +1,138 @@
+# Copyright 2017 IBM Corp. All Rights Reserved.
+# See LICENSE for details.
+#
+# Author: Henrik Loeser
+#
+# A slightly mutating ego-centric chatbot
+# Using the IBM Watson Conversation service on IBM Bluemix
+#
+# See the README for documentation.
+#
+# Partially based on this repo https://github.com/data-henrik/watson-conversation-tool
+
+import json, argparse
+from os.path import join, dirname
+from watson_developer_cloud import ConversationV1
+
+# Credentials are read from a file
+with open("config.json") as confFile:
+     config=json.load(confFile)['credentials']
+
+# Initialize the Conversation client
+conversation = ConversationV1(
+    username=config['username'],
+    password=config['password'],
+    version=config['version'],
+    url=config['url']
+    )
+
+
+# Define parameters that we want to catch and some basic command help
+def getParameters(args=None):
+    parser = argparse.ArgumentParser(description='A slightly mutating ego-centric chatbot',
+                                     prog='egobot.py',
+                                     usage='%(prog)s [-h | -l | -logs | -dialog] [-id workspaceID]')
+    parser.add_argument("-l",dest='listWorkspaces', action='store_true', help='list workspaces')
+    parser.add_argument("-logs",dest='listLogs', action='store_true', help='list logs')
+    parser.add_argument("-dialog",dest='dialog', action='store_true', help='have dialog')
+    parser.add_argument("-id",dest='workspaceID', help='Workspace ID')
+
+    parms = parser.parse_args()
+    return parms
+
+# List available dialogs
+def listWorkspaces():
+    print(json.dumps(conversation.list_workspaces(), indent=2))
+
+# List logs for a specific workspace by ID
+# For now just dump them, do not filter, do not store
+def listLogs(workspaceID):
+    print(json.dumps(conversation.list_logs(workspace_id=workspaceID), indent=2))
+
+def listIntents(workspaceID):
+    print(json.dumps(conversation.list_intents(workspace_id=workspaceID,export=False)['intents'], indent=2))
+
+def listEntities(workspaceID):
+    print(json.dumps(conversation.list_entities(workspace_id=workspaceID,export=False)['entities'], indent=2))
+
+# Add new intent after gathering the necessary data
+def addIntent(workspaceID, localizationConf):
+    # Get intent name, description and examples
+    inputForIntent = raw_input(localizationConf['intents']['iname']+"\n")
+    descForIntent = raw_input(localizationConf['intents']['idescription']+"\n")
+    examplesForIntent = raw_input(localizationConf['intents']['isamples']+"\n").split('/')
+
+    # Create JSON array from examples
+    samples=[]
+    for num in range (0, len(examplesForIntent)):
+        samples.append({"text": examplesForIntent[num]})
+
+    # Now create the intent and print the JSON response
+    print(json.dumps(conversation.create_intent(workspace_id=workspaceID,
+                        intent=inputForIntent,
+                        description=descForIntent,
+                        examples=samples),
+                         indent=2))
+
+
+# Start a dialog and converse with Watson
+#
+def converse(workspaceID):
+    # Fetch language of workspace
+    lang=conversation.get_workspace(workspace_id=workspaceID)['language']
+
+    # Now open localization file for chat language
+    fname="lang/"+lang+".json"
+    with open(fname) as localizationFile:
+        localizationConf=json.load(localizationFile)
+    print localizationConf['chat']['initialMessage']+"\n"
+    print "=========================\n"
+    # Start the dialog
+    context={}
+
+    # Now loop to chat
+    while True:
+        # get some input
+        minput = raw_input(localizationConf['chat']['prompt']+"\n")
+        # if we catch a "bye" then exit
+        if (minput == "bye"):
+            break
+        # send the input to Watson
+        resp=conversation.message(workspace_id=workspaceID,
+                             message_input={'text': minput},
+                             alternate_intents=True,
+                             context=context,
+                             entities=None,
+                             intents=None,
+                             output=None)
+
+        print(json.dumps(resp))
+        print
+        if resp['intents'][0]['intent']=="Display":
+            print resp['output']['text'][0]
+            if resp['context']['action']:
+                if resp['context']['action']=="listIntents":
+                    listIntents(workspaceID)
+                elif resp['context']['action']=="listEntities":
+                    listEntities(workspaceID)
+        elif resp['intents'][0]['intent']=="Create":
+            print resp['output']['text'][0]
+            if resp['context']['action']=="addIntent":
+                addIntent(workspaceID,localizationConf)
+
+        # Save context for next round, but wipe out the action
+        context=resp['context']
+        context['action']=""
+
+#
+# Main program, for now just detect what function to call and invoke it
+#
+if __name__ == '__main__':
+    parms = getParameters()
+    #print parms
+    if (parms.listWorkspaces):
+        listWorkspaces()
+    if (parms.listLogs and parms.workspaceID):
+        listLogs(parms.workspaceID)
+    if (parms.dialog and parms.workspaceID):
+        converse(parms.workspaceID)
